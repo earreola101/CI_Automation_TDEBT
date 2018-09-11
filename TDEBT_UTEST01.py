84555b0a52a1dc9e6d60b73ada611d25c29bb623
Edward Arreola <earreola@logitech.com>
2018-09-11 13:07:40 -0700

    updated

---
+++ TDEBT_UTEST01.py
@@ -0,0 +1,41 @@
+import subprocess
+import sys,string,os
+import serial
+import random
+import unittest
+import xmlrunner
+from timeit import default_timer
+from time import sleep
+
+#automated TDE mode test version 0.1.0
+
+tdebt="./tdebt_148.exe"
+
+class TDE_Modes(unittest.TestCase):
+    
+    #test_SetMacaddressBT
+    def test_SetMacaddressBT(self):
+        print("TEST_tdebt_SetMacaddress ")
+        cmdStr=tdebt+" -s macaddress 001122334455"
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        r = r.replace("\r\n", " ")
+        r = r.replace(" ", " ")
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual(" MacAddress=001122334455[OK]",r)
+        return
+
+        
+    #test_GetMacaddressBT
+    def test_GetMacaddressBT(self):
+        print("TEST GetMacaddress")
+        cmdStr=tdebt+" -g macaddress "
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual("001122334455",r)
+        return
+        
+if __name__ == '__main__':
+    with open('results.xml', 'wb') as output:
+        unittest.main(
+            testRunner=xmlrunner.XMLTestRunner(output=output),
+            failfast=False, buffer=False, catchbreak=False)
