f2fec202b15405e04068d893d746135a401c7416
Edward Arreola <earreola@logitech.com>
2018-10-11 14:57:38 -0700

    udpated

--- tdebt_utest_02.py
+++ tdebt_utest_02.py
@@ -13,9 +13,9 @@ tdebt="./tdebt.exe"
 
 class TDE_Modes(unittest.TestCase):
     
-    #test_SetMacaddressBT-TDETool.exe -c tvhub -s macaddress 111234567899
-    def test_SetMacaddressBT(self):
-        print("TEST_tdebt_SetMacaddress ")
+    #TEST_SetMacaddressBT-Tdebt.exe -c tvhub -s macaddress 111234567899
+    def TEST_SetMacaddressBT(self):
+        print("TEST_SetMacaddressBT ")
         cmdStr=tdebt+" -c tvhub -s macaddress 111234567899"
         r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
         r = r.replace("\r\n", " ")
@@ -24,67 +24,142 @@ class TDE_Modes(unittest.TestCase):
         self.assertEqual("[OK]",r)
         return
 		
-    #test_GetMacaddressBT
-    def test_GetMacaddressBT(self):
-        print("TEST GetMacaddress")
+    #TEST_GetMacaddressBT-Tdebt.exe -c tvhub -g macaddress
+    def TEST_GetMacaddressBT(self):
+        print("TEST_GetMacaddressBT")
         cmdStr=tdebt+" -c tvhub -g macaddress "
         r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
         print("OUTPUT: "+r+"\r\n")
         self.assertEqual("111234567899",r)
         return
    
-
-    #tdebt.exe -c tvhub -g fwversion
-    def test_Getfwversion(self):
-        print("test_Getfwversion")
-        cmdStr=tdebt+" -c tvhub -g fwversion "
+    #TEST_Settvhub_ble_peripheral_address-tdebt.exe -c tvhub -s tvhub_ble_peripheral_address 4467e23d4686
+    def TEST_Settvhub_ble_peripheral_address(self):
+        print("TEST_tdebt_SetMacaddress ")
+        cmdStr=tdebt+" -c tvhub -s tvhub_ble_peripheral_address 4467e23d4686"
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        r = r.replace("\r\n", " ")
+        r = r.replace(" ", " ")
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual("[OK]",r)
+        return
+		
+    #test_GetMacaddressBT-Tdebt.exe -c tvhub -g tvhub_ble_peripheral_address
+    def TEST_Gettvhub_ble_peripheral_address(self):
+        print("TEST_Gettvhub_ble_peripheral_address")
+        cmdStr=tdebt+" -c tvhub -g tvhub_ble_peripheral_address "
         r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
         print("OUTPUT: "+r+"\r\n")
         self.assertEqual("111234567899",r)
         return
-		
-
-    #tdebt.exe -c tvhub -s tde_unique_id 1111222233334444
-    def test_SetTde_unique_id(self):
-        print("test_SetTde_unique_id ")
+	
+    #test_Settde_unique_id-Tdebt.exe -c tvhub -s tde_unique_id 1111222233334444
+    def test_Settde_unique_id(self):
+        print("test_Settde_unique_id ")
         cmdStr=tdebt+" -c tvhub -s tde_unique_id 1111222233334444"
         r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
         r = r.replace("\r\n", " ")
         r = r.replace(" ", " ")
         print("OUTPUT: "+r+"\r\n")
         self.assertEqual("[OK]",r)
-        return		
-		
-    #tdebt.exe -c tvhub -s serialnumber 111111111222222222
-    def test_Setserialnumber(self):
-        print("test_Setserialnumber ")
-        cmdStr=tdebt+" -c tvhub -s serialnumber 111111111222222222"
-        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
-        r = r.replace("\r\n", " ")
-        r = r.replace(" ", " ")
-        print("OUTPUT: "+r+"\r\n")
-        self.assertEqual("[OK]",r)
-        return	
-		
-		
-		
-    #tdebt.exe -c tvhub -g tvhub_valens_status
-    def test_Gettvhub_valens_status(self):
-        print("test_tvhub_valens_status")
-        cmdStr=tdebt+" -c tvhub -g tvhub_valens_status"
-        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
-        print("OUTPUT: "+r+"\r\n")
-        self.assertEqual("111234567899",r)
-        return		
-
-    #tdebt.exe -c tvhub -g tvhub_ble_status
-    def test_GetTvhub_ble_status(self):
-        print("test_GetTvhub_ble_status")
-        cmdStr=tdebt+" -c tvhub -g tvhub_ble_status"
-        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
-        print("OUTPUT: "+r+"\r\n")
-        self.assertEqual("111234567899",r)
         return
+		
+	#test_GetTde_unique_id-Tdebt.exe -c tvhub -g tde_unique_id
+    def TEST_Gettde_unique_id(self):
+        print("test_GetTde_unique_id")
+        cmdStr=tdebt+" -c tvhub -g tde_unique_id"
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual("1111222233334444",r)
+        return	
+   
+
+   	#test_Gettvhub_cec_status-tdebt.exe -c tvhub -g tvhub_cec_status
+    def test_Gettvhub_cec_status(self):
+        print("test_Gettvhub_cec_status")
+        cmdStr=tdebt+" -c tvhub -g tvhub_cec_status"
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual("0",r)
+        return
+
+   	#TEST_Gettvhub_chrontel_status-tdebt.exe -c tvhub -g tvhub_chrontel_status
+    def TEST_Gettvhub_chrontel_status(self):
+        print("TEST_Gettvhub_chrontel_status")
+        cmdStr=tdebt+" -c tvhub -g tvhub_chrontel_status"
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual("0",r)
+        return	
+    
+   	#TEST_Gettvhub_ble_peripheral_address-tdebt.exe -c tvhub -s tvhub_ble_peripheral_address 4467e23d4686
+    def TEST_Gettvhub_ble_peripheral_address(self):
+        print("test_GetTde_unique_id")
+        cmdStr=tdebt+" -c tvhub -s tvhub_ble_peripheral_address 4467e23d4686"
+        r=subprocess.check_output(cmdStr,stderr=subprocess.STDOUT)
+        print("OUTPUT: "+r+"\r\n")
+        self.assertEqual("0",r)
+        return	
+    
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	#tdebt.exe -c tvhub -g tvhub_ble_peripheral_address
+
+	#tdebt.exe -c tvhub -s tvhub_ble_central_address 4467e23d4686
+
+	#tdebt.exe -c tvhub -g tvhub_ble_central_address
+
+	#tdebt.exe -c tvhub -s tvhub_ble_camera_address eedd55e83755
+
+	#tdebt.exe -c tvhub -g tvhub_ble_camera_address
+
+    #tdebt.exe -c tvhub -s tde_test_tracking_data 11112222333344445555666677778888
+
+    #tdebt.exe -c tvhub -g tde_test_tracking_data
+
+	#tdebt.exe -c tvhub -s macaddress 4467e23d4686
+
+	#tdebt.exe -c tvhub -g macaddress
+
+	#tdebt.exe -c tvhub -s radiotxdata
+
+	#tdebt.exe -c tvhub -s bt_uartdisable
+
+	#tdebt.exe -c tvhub -s pskey 36
+
+	#tdebt.exe -c tvhub -g pskey
+
+	#tdebt.exe -c tvhub -s tvhub_led 1
+
+	#tdebt.exe -c tvhub -s tvhub_led 0
+
+	#tdebt.exe -c tvhub -s tvhub_spplib_bypass 1  (bypass)
+
+	#tdebt.exe -c tvhub -s tvhub_spplib_bypass 0  (not bypass)
+
+	#tdebt.exe -c tvhub -g btn_status		
 
 if __name__ == '__main__':
     with open('results.xml', 'wb') as output:
